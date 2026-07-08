2c9673c568a820902eafcf96bf87904b38b4cada
Jordan Benjamin <jordantbenjamin@gmail.com>
2026-07-08 04:53:27 -0700

    fix 1.11 1.10 CI alloc test

--- test/allocations.jl
+++ test/allocations.jl
@@ -7,7 +7,17 @@ using SOCRATESSingleColumnForcings: SOCRATESSingleColumnForcings as SSCF
 # function barrier `_eval` keeps the measurement free of global-scope boxing; each interpolant is
 # warmed up (compiled) before `@allocated`.
 # ---------------------------------------------------------------------------
-_eval(itp, x) = itp(x)
+#=
+    see https://discourse.julialang.org/t/allocations-on-field-access-even-though-no-abstract-types-are-involved/126672/6
+    On 1.12 things like
+        Test.@test @allocated(ss(2.5)) == 0
+    do not allocate, but the field accesses inside the interpolant do seem to allocate a 16 byte single allocation on 1.11
+    This form does not allocate, and we believe this shows it is only a global context lowering problem
+    (also defining the interpolants su, si, ss as cosnts would fix, let block does not either, etc...)
+=#
+function allocation_test(itp, x) # https://discourse.julialang.org/t/allocations-on-field-access-even-though-no-abstract-types-are-involved/126672/6
+    @allocated itp(x)
+end
 
 Test.@testset "hot-path eval is allocation-free" begin
     I = SSCF.Interpolation
@@ -17,9 +27,10 @@ Test.@testset "hot-path eval is allocation-free" begin
     si = I.build_spline(I.FastLinear1DInterpolation, [0.0, 1.0, 3.0, 7.0, 15.0, 31.0], cos.([0.0, 1.0, 3.0, 7.0, 15.0, 31.0]); bc = ext, drop_collinear = false)  # Vector, irregular (search)
     ss = I.build_spline(I.FastLinear1DInterpolation, I.create_svector(collect(0.0:1.0:9.0)), I.create_svector(collect(0.0:2.0:18.0)); bc = ext, drop_collinear = false)  # SVector, isbits
 
-    _eval(su, 7.3); _eval(si, 5.0); _eval(ss, 2.5)   # warm up / compile
+    su(7.3); si(5.0); ss(2.5)   # warm up / compile
+    allocation_test(su, 7.3); allocation_test(si, 5.0); allocation_test(ss, 2.5); 
 
-    Test.@test @allocated(_eval(su, 7.3)) == 0
-    Test.@test @allocated(_eval(si, 5.0)) == 0
-    Test.@test @allocated(_eval(ss, 2.5)) == 0
+    Test.@test allocation_test(su, 7.3) == 0
+    Test.@test allocation_test(si, 5.0) == 0
+    Test.@test allocation_test(ss, 2.5) == 0
 end
